5269f9c49ffb2281ad3fcfaa81665166feea1c5f
Akisy332 <94855230+Akisy332@users.noreply.github.com>
2024-09-05 13:15:15 +0700

    Add files via upload

---
+++ Compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get install autoconf automake build-essential ccache cmake cpufrequtils doxygen ethtool \
+g++ git inetutils-tools libboost-all-dev libncurses5 libncurses5-dev libusb-1.0-0 libusb-1.0-0-dev \
+libusb-dev python3-dev python3-mako python3-numpy python3-requests python3-scipy python3-setuptools \
+python3-ruamel.yaml 
+sudo ldconfig
+cd host
+mkdir build
+cd build
+cmake ../
+make -j4
